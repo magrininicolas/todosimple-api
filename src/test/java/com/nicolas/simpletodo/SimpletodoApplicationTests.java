98462e05eef81ffe9f8d14b10556e66798202d33
Nicolas <magrininicolas@outlook.com>
2023-09-15 13:56:44 -0300

    Initial Project

---
+++ src/test/java/com/nicolas/simpletodo/SimpletodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolas.simpletodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpletodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
