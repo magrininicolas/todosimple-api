d53eb87aee92a97c1672d29f68e2033ba0cd9ac5
Nicolas <magrininicolas@outlook.com>
2023-10-11 15:03:50 -0300

    Refactor UserController.java

--- src/main/java/com/nicolas/simpletodo/controllers/UserController.java
+++ src/main/java/com/nicolas/simpletodo/controllers/UserController.java
@@ -60,7 +60,7 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.delete(id);
 
         return ResponseEntity.noContent().build();
