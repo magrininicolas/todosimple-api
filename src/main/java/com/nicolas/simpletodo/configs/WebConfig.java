292dd0d2c3aab4f6fc81749831cdbdac484adf40
Nicolas <magrininicolas@outlook.com>
2023-10-13 16:05:27 -0300

    Add WebConfig.java for CORS configuration

--- src/main/java/com/nicolas/simpletodo/configs/WebConfig.java
+++ src/main/java/com/nicolas/simpletodo/configs/WebConfig.java
@@ -3,6 +3,7 @@ package com.nicolas.simpletodo.configs;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
