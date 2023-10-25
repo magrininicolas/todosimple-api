bd765be65163e77b54a2826c8bb63a13ad827d14
Nicolas <magrininicolas@outlook.com>
2023-10-24 23:53:21 -0300

    Add Exceptions Handler to the project

---
+++ src/main/java/com/nicolas/simpletodo/services/exceptions/DataBindingViolationException.java
@@ -0,0 +1,13 @@
+package com.nicolas.simpletodo.services.exceptions;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class DataBindingViolationException extends DataIntegrityViolationException {
+
+    public DataBindingViolationException(String message) {
+        super(message);
+    }
+}
